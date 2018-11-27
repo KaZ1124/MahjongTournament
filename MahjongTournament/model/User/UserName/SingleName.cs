d7d50430261a7e798204aa3e5c33301141056de0
KaZ1124 <HiraKazu1124@gmail.com>
2018-11-27 21:16:29 +0900

    UserNameの作成

---
+++ MahjongTournament/model/User/UserName/SingleName.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MahjongTournament.model.User.UserName {
+    /// <summary>
+    /// 単一の名前（入力されたまま氏名を表示する）
+    /// </summary>
+    public class SingleUserName : UserName {
+        private string single = null;
+
+        /// <summary>
+        /// 氏名
+        /// </summary>
+        public string Single {
+            get => single ?? string.Empty;
+            set => single = DeleteCharSide(value) ?? string.Empty;
+        }
+
+        [XmlIgnore()]
+        public override string Name {
+            get => Single;
+            set => Single = value;
+        }
+
+        public SingleUserName() { }
+
+        public SingleUserName(string name) {
+            Name = name;
+        }
+    }
+}
